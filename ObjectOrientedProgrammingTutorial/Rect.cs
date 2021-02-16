81d286e5356900baefdacb01c47c8571afbf9122
max-student <max-student@maxtrain.com>
2021-02-16 16:10:36 -0500

    Add project files.

---
+++ ObjectOrientedProgrammingTutorial/Rect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgrammingTutorial {
+    class Rect : Quad{
+
+        public int Area() {
+            return Side1 * Side2;
+        }
+
+        public Rect() { }
+
+        public Rect(int s1 , int s2) : base(s1, s2, s1, s2) { 
+        
+        }
+    }
+}
---
+++ ObjectOrientedProgrammingTutorial/Sqr.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrientedProgrammingTutorial {
+
+    class Sqr : Rect {
+
+        public Sqr() { }
+
+        public Sqr(int s1) : base(s1, s1) { }
+
+        
+
+        
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
